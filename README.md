8286cbbb9e628bc5733561d922f7812db39a9f7e
Harold Kim <me@harold.kim>
2019-08-16 03:58:50 +0900

    Fix build Script, Add 404, Layout update

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
-# r3kapig CTF writeups
+## Welcome to our writeup!
 
-r3kapig is a united CTF Team mostly emerges from Eur3kA and FlappyPig since 
+r3kapig is a united CTF Team mostly emerges from Eur3kA and FlappyPig since 2018.
 
-For more information, please refer to [our website](https://r3kapig.com).
+r3kapig is a delicious dish that can be grilled and fried, and the mission of the team is to provide the most delicious food for the host. :P
 
-## Table of Contents
+We are publishing our official writeups here. For more information, please refer to [our website](https://r3kapig.com).
 
+## Writeups
 
 - [20190323-0ctf](20190323-0ctf)
 - [20190528-qwb](20190528-qwb)
 - [20190601-defconchina](20190601-defconchina)
 - [20190617-qwbfinal](20190617-qwbfinal)
 
-
 ## Questions
 
 If you have any question regarding our writeups, please feel free to [create an issue](https://github.com/r3kapig/writeup/issues) in the writeup repository.
-
--- build.sh
+++ build.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
+# Description: Bash golfing build script by stypr
+# Maintainer: Harold Kim (root@stypr.com)
+
 
 DIR_LIST=`ls | grep -P '\d{8}-' --color=none`
 
-echo "Building..."
+# Build writeup
+echo "Building Writeups..."
 for DIR in $DIR_LIST
 do
+    # generate markdown
     ./util/markdown-to-html/node_modules/markdown-styles/bin/generate-md --layout r3kapig --input $DIR/README.md --output $DIR
     mv $DIR/README.html $DIR/index.html
     ./util/markdown-to-html/gen-sidebar.py $DIR/index.html
-    ./util/markdown-toc-generator/gen-toc.py $DIR/index.html
+    # do we need this file?
+    # ./util/markdown-toc-generator/gen-toc.py $DIR/README.md 2>&1
 done
 
-# ./util/markdown-toc-generator/gen-toc.py
-# last dir will be taken
-#echo "Generating Index..."
-# mv $DIR/README.html ..
+
+# Update writeup list (README.md)
+echo "Updating Writeup list..."
+SIDEBAR_START=$(expr `sed -n '/##\ Writeups/=' README.md` + 1)
+SIDEBAR_END=$(expr `sed -n '/##\ Questions/=' README.md` - 1)
+LAST_LINE=`cat README.md | wc -l`
+cat /dev/null > README_patched.md
+# Write to patched
+sed -n "1,${SIDEBAR_START}p;${SIDEBAR_START}q" README.md >> README_patched.md
+for DIR in $DIR_LIST
+do
+    echo "- [$DIR]($DIR)" >> README_patched.md
+done
+sed -n "${SIDEBAR_END},${LAST_LINE}p;${LAST_LINE}q" README.md >> README_patched.md
+mv README_patched.md README.md
+
+# Generate index.html from README.md
+echo "Generating Index..."
+./util/markdown-to-html/node_modules/markdown-styles/bin/generate-md --layout r3kapig --input README.md --output .
+mv README.html index.html
+./util/markdown-to-html/gen-sidebar.py index.html
+
+# remove asset bug, remove sidebar
+sed -i 's/\.\.\//\.\//g' index.html
+SIDEBAR_START=$(expr `sed -n '/\"sidebar\-container\"/=' index.html` - 1)
+SIDEBAR_END=$(expr `sed -n '/\"col-10\ py-3\"/=' index.html`)
+LAST_LINE=`cat index.html | wc -l`
+sed -n "1,${SIDEBAR_START}p;${SIDEBAR_END},${LAST_LINE}p;${LAST_LINE}q" index.html > patched.html
+sed -i 's/col-10\ py-3/col-12\ py-3/g' patched.html
+mv patched.html index.html
